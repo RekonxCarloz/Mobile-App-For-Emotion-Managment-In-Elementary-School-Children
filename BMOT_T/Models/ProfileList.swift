7e8b499cb4880e57bf7f0dc66d5c731ce52d15ee
Carlos Cobian <rcobianm1600@alumno.ipn.x>
2021-11-03 13:30:46 -0600

    Se crearon los modelos de la lista de perfiles

---
+++ BMOT_T/Models/ProfileList.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileSettings.swift
+//  BMOT_T
+//
+//  Created by Carlos Cobian on 02/11/21.
+//
+
+import UIKit
+
+struct ProfileList{
+    let nombre: String
+    let color: UIColor
+    let avatar: UIImage
+}
--- BMOT_T/Models/ProfileSettings.swift
+++ BMOT_T/Models/ProfileSettings.swift
@@ -2,14 +2,15 @@
 //  ProfileSettings.swift
 //  BMOT_T
 //
-//  Created by Carlos Cobian on 02/11/21.
+//  Created by Carlos Cobian on 03/11/21.
 //
 
+import Foundation
 import UIKit
 
 struct ProfileSettings{
-    let nombre: String
-    let edad: Int
-    let color: UIColor
+    let name: String
+    let age: Int
     let avatar: UIImage
+    let color: UIColor
 }

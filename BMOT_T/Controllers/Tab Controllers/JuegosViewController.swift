a06653716dd00893d9a03a77e03a18540ffe1c37
Carlos Cobian <rcobianm1600@alumno.ipn.x>
2021-11-19 20:41:40 -0600

    perform Segue de cada juego

--- BMOT_T/Controllers/Tab Controllers/JuegosViewController.swift
+++ BMOT_T/Controllers/Tab Controllers/JuegosViewController.swift
@@ -16,4 +16,46 @@ class JuegosViewController: UIViewController {
         
         navigationItem.hidesBackButton = true
     }
+    
+    
+    
+    @IBAction func wordSearcherButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: K.Segues.wordSearcherGameToEmotions, sender: self)
+    }
+    
+    
+    @IBAction func memoramaButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: K.Segues.memoramaGameToEmotions, sender: self)
+    }
+    
+    
+    @IBAction func tictactoeButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: K.Segues.ticTacToeGameToEmotions, sender: self)
+    }
+    
+    @IBAction func pizzaButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: K.Segues.pizzaGameSegue, sender: self)
+    }
+    
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == K.Segues.wordSearcherGameToEmotions{
+            let vc = segue.destination as! SelectEmotionWordSearcherViewController
+            vc.nombrePerfil = nombrePerfil
+            
+        }else if segue.identifier == K.Segues.memoramaGameToEmotions{
+            let vc = segue.destination as! SelectEmotionMemoramaViewController
+            vc.nombrePerfil = nombrePerfil
+            
+        }else if segue.identifier == K.Segues.ticTacToeGameToEmotions{
+            let vc = segue.destination as! TictactoeFichasViewController
+            vc.nombrePerfil = nombrePerfil
+            
+        }else if segue.identifier == K.Segues.pizzaGameSegue{
+            let vc = segue.destination as! PizzaGameViewController
+            vc.nombrePerfil = nombrePerfil
+        }
+    }
+    
 }
